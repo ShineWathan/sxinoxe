21692072b12f0e7961584737045f43e3500407d7
Shine Wathan <shinewathan23@gmail.com>
2022-12-18 19:11:29 +0630

    by kai

---
+++ python/eg2.py
@@ -0,0 +1,13 @@
+
+user_input = str(input('What\'s the meaning of each color ?'))
+
+
+if user_input == 'green':
+    print('you can go')
+
+elif user_input == 'yellow':
+    print('wait')
+
+elif user_input == 'red':
+    print('don\'t go now, it is dangerous')
+    
