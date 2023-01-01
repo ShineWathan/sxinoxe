73e3ceb19f2802c4ef84a355a56d6ebc88054e2e
Shine Wathan <shinewathan23@gmail.com>
2023-01-01 19:53:37 +0630

    Rivan

--- python/hw.py
+++ python/hw.py
@@ -1,6 +1,7 @@
 my_math_marks = 100
 mybro_math_marks = 80
 mysis_math_marks = 83
+user_input = int(input(''))
 
 if my_math_marks > mybro_math_marks :
     print("your marks are too low bro")
@@ -12,4 +13,4 @@ elif my_math_marks == mybro_math_mark:
     print("bruhh lucky boi")
 
 else:
-    print("nc")    
+    print("nc")
---
+++ python/test.py
@@ -0,0 +1,9 @@
+user_input = int(input(''))
+if user_input >= 80 and user_input <= 100 :
+    print('A')
+elif user_input >= 60 and user_input <= 79 :
+    print('B')
+elif  user_input >= 40 and user_input <= 59 :
+    print('C')
+elif user_input < 40 :
+    print('D')
